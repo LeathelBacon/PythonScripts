18f9050e97599c78649feb92bffd7e0883902c22
Oriel <b.oriel1991@gmail.com>
2018-08-11 22:04:53 +0300

    Working Python script oriel
    Signed-off-by: Oriel <b.oriel1991@gmail.com>

---
+++ PandaMissionOriel.py
@@ -0,0 +1,50 @@
+'''
+Author: Oriel Bacdner
+Date: 11/08/18
+
+Description:
+This script downloads the tar file from the requested URL, unpacks it and execute the docker compose file and command
+
+'''
+
+#!/usr/bin/python
+import urllib
+import tarfile
+import os
+import requests
+import json
+import sys
+import time
+
+#Downloading and extracting the files locally (before copying inside the container)
+downloadFile = urllib.URLopener()
+downloadFile.retrieve("https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz", "file.tar.gz")
+tar= tarfile.open("file.tar.gz")
+tar.extractall("/public/images")
+tar.close()
+
+#The step to launch the docker-compose up (added the -d to detach or else script will not continue)
+os.system("docker-compose up -d")
+
+#Copy the directory extract in the first phase to the app container
+os.system("docker cp /public/images/ ops-exercise_app_oriel_1:/public")
+
+print 'File downloaded and extracted!!! "\n'
+
+#Need to wait a few seconds before making the health check to give time for the containers to be fully up
+time.sleep(10)
+
+#Below is the healthcheck
+response = requests.get('http://localhost:3000/health')
+data = response.json()
+if data[u'isSuccessful'] == True:
+        print 'Health check is all good and running!'
+else:
+        print 'Internal 500 error, aborting script!'
+        sys.exit()
+
+
+
+
+
+
